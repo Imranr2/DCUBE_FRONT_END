48ff3bed0d54d26a38983eea1970ce95d45ff4d2
Imran <imranr2@msn.com>
2023-09-17 17:50:01 +0800

    Use interceptor

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import SignUp from "./pages/SignUp";
 import { theme } from "./utils/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { AxiosInterceptor } from "./api/interceptor";
+import { SnackbarProvider } from "notistack";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </Router>
+      <SnackbarProvider maxSnack={3}>
+        <AxiosInterceptor>
+          <Router>
+            <Routes>
+              <Route path="/" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </Router>
+        </AxiosInterceptor>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
